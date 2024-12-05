dc94125a1deb3d999b5f4c80cd4cc78dd94ae724
mdhasanshuvo <mdhasanshuvo220@gmail.com>
2024-12-05 22:03:37 +0600

    Delete visa on mongodb

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -31,10 +31,25 @@ async function run() {
         const visaCollection = client.db('visaDB').collection('visa');
 
 
-        app.post('/visarena', async(req,res) =>{
+        app.get('/visarena', async (req, res) => {
+            const cursor = visaCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+
+        app.post('/visarena', async (req, res) => {
             const newVisa = req.body;
             console.log(newVisa);
             const result = await visaCollection.insertOne(newVisa);
+            res.send(result);
+        })
+
+
+        app.delete('/visarena/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await visaCollection.deleteOne(query);
             res.send(result);
         })
 
